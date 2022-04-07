f54f4948b1f72a8d57549dcf755cfd5f901f3549
AM0726Github <Margaryanalik7@gmail.com>
2022-04-07 15:04:50 -0700

    Server Running ok!

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Sexkret',
+    cookie: { maxAge: 86400 },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Server listening on: http://localhost:${PORT}`));
+});
