eea6237457ebd6a0fef30952adb21d8ea50a3d0f
AM0726Github <Margaryanalik7@gmail.com>
2022-04-08 10:06:13 -0700

    Handlebars Bugfix && Javascript Done App working well!

--- server.js
+++ server.js
@@ -2,33 +2,34 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3005;
 const exphbs = require('express-handlebars');
 const session = require('express-session');
-const SequelizeThat = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'Sexkret',
-    cookie: { maxAge: 86400 },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeThat({ db: sequelize })
+  secret: 'Sekret',
+  cookie: { maxAge: 86400 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
-
-const help = require('./utils/helpers');
-const hbs = exphbs.create({ help });
-
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Server listening on: http://localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`App Now listening at http://localhost:${PORT}/...`));
 });
